e82bf9a67f694550d8b4acb3088edcfb106764e6
RISHAV YADAV <132156106+RISHAVY9@users.noreply.github.com>
2025-08-06 09:06:53 +0530

    Create leetcode485.java

---
+++ leetcode485.java
@@ -0,0 +1,17 @@
+//Max Consecutive Ones | Leetcode 485
+
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int curr = 0;
+        int max = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                curr++;
+                max = Math.max(max, curr);
+            } else {
+                curr = 0;
+            }
+        }
+        return max;
+    }
+}
