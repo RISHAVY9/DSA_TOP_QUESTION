dcd709b396d137328104f01dd328263545a53fdb
RISHAV YADAV <132156106+RISHAVY9@users.noreply.github.com>
2025-08-07 09:18:30 +0530

    Create leetcode118.java

---
+++ leetcode118.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> list = new ArrayList<>();
+        List<Integer> prev = new ArrayList<>();
+        for (int i = 1; i <= numRows; i++) {
+            List<Integer> curr = new ArrayList<>();
+            for (int j = 1; j <= i; j++) {
+                if (j == 1 || j == i) curr.add(1);
+                else curr.add(prev.get(j - 2) + prev.get(j - 1));
+            }
+            prev = curr;
+            list.add(new ArrayList<>(curr));
+        }
+        return list;
+    }
+}
