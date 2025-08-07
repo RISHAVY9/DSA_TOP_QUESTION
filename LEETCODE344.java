120765cefa9efc3e931b277a92dc8d34e711f7a0
RISHAV YADAV <132156106+RISHAVY9@users.noreply.github.com>
2025-08-07 08:01:12 +0530

    Update LEETCODE344.java

--- LEETCODE344.java
+++ LEETCODE344.java
@@ -1,14 +1,25 @@
 //LeetCode #344: Reverse String java solution
 
 class Solution {
-    public void reverseString(char[] s) {
-        int start = 0, end = s.length - 1;
-        while (start < end) {
-            char tmp = s[start];
-            s[start] = s[end];
-            s[end] = tmp;
-            start++;
-            end--;
+    public int[] searchRange(int[] nums, int target) {
+        int left = firstGreaterEqual(nums, target);
+        if (left == nums.length || nums[left] != target) {
+            return new int[]{-1, -1};
         }
+        int right = firstGreaterEqual(nums, target + 1) - 1;
+        return new int[]{left, right};
+    }
+
+    private int firstGreaterEqual(int[] arr, int target) {
+        int l = 0, r = arr.length;
+        while (l < r) {
+            int mid = (l + r) / 2;
+            if (arr[mid] >= target) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return l;
     }
 }
