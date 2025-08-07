8e967762773cce9e391db7e5a9589f8797790073
RISHAV YADAV <132156106+RISHAVY9@users.noreply.github.com>
2025-08-07 08:07:54 +0530

    Create leetcode704.java

---
+++ leetcode704.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        // Optional early exit if target is outside boundaries
+        if (target < nums[left] || target > nums[right]) {
+            return -1;
+        }
+
+        while (left <= right) {
+            // Avoid overflow with mid calculation
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
