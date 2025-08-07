9ff8b012c53e7b949402833e97a0d611f0d15a60
RISHAV YADAV <132156106+RISHAVY9@users.noreply.github.com>
2025-08-07 09:20:32 +0530

    Create leetcode258.java

---
+++ leetcode258.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int addDigits(int num) {
+        // Special case: 0 remains 0
+        if (num == 0) return 0;
+        // Digital root via modulo 9
+        return (num - 1) % 9 + 1;
+    }
+}
