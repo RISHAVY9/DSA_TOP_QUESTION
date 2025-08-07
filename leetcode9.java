cab38a3334141208a65db8f3cbee663acd10a1f1
RISHAV YADAV <132156106+RISHAVY9@users.noreply.github.com>
2025-08-07 09:12:51 +0530

    Create leetcode9.java

---
+++ leetcode9.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) return false;
+        long reversed = 0;
+        int temp = x;
+        while (temp > 0) {
+            int digit = temp % 10;
+            reversed = reversed * 10 + digit;
+            temp /= 10;
+            // Overflow safety
+            if (reversed > Integer.MAX_VALUE) {
+                return false;
+            }
+        }
+        return reversed == x;
+    }
+}
