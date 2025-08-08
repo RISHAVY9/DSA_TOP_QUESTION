921bf4ab1dea6588853fe9fc67305774024b3aaf
RISHAV YADAV <132156106+RISHAVY9@users.noreply.github.com>
2025-08-08 07:42:23 +0530

    Update leetcode121.java

--- leetcode121.java
+++ leetcode121.java
@@ -1,5 +1,10 @@
 //121.BEST TIME TO SELL AND BUY STOCKS
 
+https://www.youtube.com/watch?v=jQDIeuMIPGI&t=404s&pp=ygUcbGVldGNvZGUgcHJvYmxlbSBpbiBqYXZhIDEyMQ%3D%3D
+
+
+
+
 class Solution {
     public int maxProfit(int[] prices) {
         if (prices == null || prices.length == 0) return 0;
