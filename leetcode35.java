fe33da02d5cde45e81db45dff43bda54711348b6
RISHAV YADAV <132156106+RISHAVY9@users.noreply.github.com>
2025-08-11 09:56:09 +0530

    Create leetcode35.java

---
+++ leetcode35.java
@@ -0,0 +1,18 @@
+https://www.youtube.com/watch?v=SW4jN-Fvw1g
+
+
+
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length;
+        while (left < right) {
+            int mid = (left + right) >>> 1;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+}
