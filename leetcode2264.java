c1465a286eba4e66df9ce604c0693ea6f3d43391
RISHAV YADAV <132156106+RISHAVY9@users.noreply.github.com>
2025-08-14 08:17:00 +0530

    Create leetcode2264.java

---
+++ leetcode2264.java
@@ -0,0 +1,19 @@
+https://www.youtube.com/watch?v=gfnb3a39IZg
+
+
+class Solution {
+    public String largestGoodInteger(String num) {
+        String maxGood = "" ;
+        for(int i = 0 ; i <=num.length()-3 ; i++){
+            String sub = num.substring(i , i+3);
+            if ( sub.charAt(0)==sub.charAt(1) && sub.charAt(1)==sub.charAt(2)){
+                if (maxGood.equals("") || sub.compareTo(maxGood)> 0){
+                    maxGood = sub;
+                }
+            }
+
+        }
+        return maxGood ;
+
+    }
+}
