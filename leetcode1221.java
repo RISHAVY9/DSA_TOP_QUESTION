b7386b42414430e2e5a06adf7f354a30d4505fd9
RISHAV YADAV <132156106+RISHAVY9@users.noreply.github.com>
2025-08-22 07:35:41 +0530

    Create leetcode1221.java

---
+++ leetcode1221.java
@@ -0,0 +1,14 @@
+https://youtu.be/JeQD5ILMDa0
+
+
+class Solution {
+    public int balancedStringSplit(String s) {
+        int left = 0, right = 0, count = 0;
+        for (char c : s.toCharArray()) {
+            if (c == 'L') left++;
+            if (c == 'R') right++;
+            if (left == right) count++;
+        }
+        return count;
+    }
+}
