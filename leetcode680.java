eca4d737af5993a6ed2aeea66526c88066c28893
RISHAV YADAV <132156106+RISHAVY9@users.noreply.github.com>
2025-08-22 08:00:47 +0530

    Create leetcode680.java

---
+++ leetcode680.java
@@ -0,0 +1,29 @@
+https://youtu.be/wX3-411uJH0
+
+
+class Solution {
+    public boolean validPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                // Try skipping either the left or right character
+                return isPalindrome(s, left + 1, right) ||
+                       isPalindrome(s, left, right - 1);
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    private boolean isPalindrome(String s, int start, int end) {
+        while (start < end) {
+            if (s.charAt(start) != s.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
