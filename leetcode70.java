16b94c2cdc5b7cf0555a78c0f8c19ca9630fa885
RISHAV YADAV <132156106+RISHAVY9@users.noreply.github.com>
2025-08-31 17:02:49 +0530

    Create leetcode70.java

---
+++ leetcode70.java
@@ -0,0 +1,16 @@
+https://youtu.be/cGTFd5MbJ_4
+
+class Solution {
+    public int climbStairs(int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+
+        int a = 1, b = 2, res = 0;
+        for (int i = 3; i <= n; i++) {
+            res = a + b;
+            a = b;
+            b = res;
+        }
+        return res;
+    }
+}
