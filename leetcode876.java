e2b5d0d0d14e028bb25c07ab39c83cb57bbf3d3e
RISHAV YADAV <132156106+RISHAVY9@users.noreply.github.com>
2025-09-03 07:13:06 +0530

    Create leetcode876.java

---
+++ leetcode876.java
@@ -0,0 +1,14 @@
+https://youtu.be/NBRCtI-7-84
+
+
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
