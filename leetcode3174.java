59680c2a4bcea5811c69539f8f735f3ee91943a0
RISHAV YADAV <132156106+RISHAVY9@users.noreply.github.com>
2025-09-05 08:37:38 +0530

    Create leetcode3174.java

---
+++ leetcode3174.java
@@ -0,0 +1,15 @@
+https://youtu.be/SggoqMRCznk
+
+class Solution {
+    public String clearDigits(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (Character.isDigit(c)) {
+                sb.setLength(sb.length() - 1);
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+}
