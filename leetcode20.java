6b679fe8ac9aede69266f0cbc06ff36a2c74a0a2
RISHAV YADAV <132156106+RISHAVY9@users.noreply.github.com>
2025-09-09 11:27:16 +0530

    Create leetcode20.java

---
+++ leetcode20.java
@@ -0,0 +1,17 @@
+
+
+
+import java.util.ArrayDeque;
+
+class Solution {
+    public boolean isValid(String s) {
+        ArrayDeque<Character> stack = new ArrayDeque<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(') stack.push(')');
+            else if (c == '{') stack.push('}');
+            else if (c == '[') stack.push(']');
+            else if (stack.isEmpty() || stack.pop() != c) return false;
+        }
+        return stack.isEmpty();
+    }
+}
